aca25e014a1038128d00462f8c3157d47e67ec44
Egene Joshua <92093251+egenejosh@users.noreply.github.com>
2022-09-08 20:16:52 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
- *
- *  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
- *
- *   * Return: Always 0 (success)
- *
+ * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
+ *   * Return: Always 0 (success
  *    */
 int main(void)
 {
