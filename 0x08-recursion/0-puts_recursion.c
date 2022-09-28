86bc29ed85e6b1f124a53ca7d5e1ccd81742868e
egenejosh <egenejoshua>
2022-09-28 07:24:39 -0700

    Aded first file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string*
+ * @s: pointer to the char array which :wq
+ * is the string
+ * Retur
+
+{
+
+	if (*s != '\0')
+
+		{
+
+			    _putchar(*s);
+
+				    _puts_recursion(s + 1);
+
+					}
+
+	    else
+
+		    {
+
+				_putchar('\n');
+
+				    }
+
+}
