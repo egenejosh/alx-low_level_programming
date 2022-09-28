0d41bc6da5a310afc9eadd70fed39c97f9449b25
egenejosh <egenejoshua>
2022-09-28 07:30:06 -0700

    Adding Recursive files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,29 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string*
- * @s: pointer to the char array which :wq
- * is the string
- * Retur
+ * _puts_recursion - Prints a string
+ * @s: pointer to the char array which is the string
+ * Return: void
+ */
 
+void _puts_recursion(char *s)
 {
-
 	if (*s != '\0')
-
-		{
-
-			    _putchar(*s);
-
-				    _puts_recursion(s + 1);
-
-					}
-
-	    else
-
-		    {
-
-				_putchar('\n');
-
-				    }
-
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
